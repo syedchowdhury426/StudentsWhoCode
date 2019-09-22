cde8983b53abd76ff043536027434430d1fff8c3
Kentro-lms <syedchowdhury426@live.com>
2019-09-22 05:44:21 -0400

    added documentation

--- code-landing-page/src/components/Home.js
+++ code-landing-page/src/components/Home.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 
+/*
+  Home is the starting page 
+  displays branding image 
+  brief information on what we do 
+*/
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -10,12 +15,17 @@ class Home extends Component {
     return (
       <div style={imageContainer}>
         <img style={image} src={require("../resources/background.jpg")} />
-        <p style={centerText}>HELLO</p>
+        <p style={centerText}>Students Who Code</p>
       </div>
     );
   }
 }
 
+/*
+  style for box that contains the image
+  used to set the image to be in the center 
+  cuts off the rest of the image that overflows past the page 
+*/
 const imageContainer = {
   display: "flex",
   justifyContent: "center",
@@ -24,17 +34,29 @@ const imageContainer = {
   overflow: "hidden"
 };
 
+/*
+  style for image 
+  used to set a maxWidth, so for future images it never gets too big 
+*/
 const image = {
   maxWidth: window.innerWidth
 };
 
+/*
+  style for the container of the center text 
+  used to position text onto image 
+  creates shape for background 
+*/
 const centerText = {
   position: "absolute",
-  top: "50%",
+  top: "40%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  backgroundColor: "white",
-  padding: "50px"
+  color: "white",
+  backgroundColor: "black",
+  borderRadius: "10% / 50%",
+  fontSize: "1.5em",
+  padding: "5vmin"
 };
 
 export default Home;
