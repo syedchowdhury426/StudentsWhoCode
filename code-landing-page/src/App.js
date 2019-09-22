1f51fbdf1f877638e334b54c2553e4a23cc6e700
Kentro-lms <syedchowdhury426@live.com>
2019-09-22 04:08:37 -0400

    created responsive header. added link routing as well

--- code-landing-page/src/App.js
+++ code-landing-page/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Classes from "./components/Classes";
--- code-landing-page/src/components/Header.js
+++ code-landing-page/src/components/Header.js
@@ -1,14 +1,79 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+
+const Header = props => {
+  const { classes } = props;
+
+  return (
+    <AppBar className={classes.appBar}>
+      {/* Toolbar allows for all elements within to be shown horizontally */}
+      <Toolbar>
+        <Typography variant="h6" className={classes.titleContainer}>
+          <Button
+            onClick={() => props.history.push("/")}
+            className={classes.titleLabel}
+          >
+            Students Who Code
+          </Button>
+        </Typography>
+        <Button
+          onClick={() => props.history.push("/classes")}
+          className={classes.classes}
+        >
+          Classes
+        </Button>
+        <Button
+          onClick={() => props.history.push("/contact")}
+          className={classes.contact}
+        >
+          Contact
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+const styles = () => ({
+  appBar: {
+    flexGrow: 1,
+    left: "0%",
+    top: "0%",
+    width: "100%",
+    boxShadow: "none",
+    position: "absolute",
+    backgroundColor: "#FFF",
+    border: "2px solid black"
+    // backgroundColor: "rgb(62, 118, 193)"
+  },
+  titleContainer: {
+    flexGrow: 1,
+    position: "relative"
+  },
+  titleLabel: {
+    fontSize: "1em",
+    position: "relative"
+  },
+  classes: {
+    fontSize: "1em",
+    position: "relative",
+    marginRight: "2%"
+  },
+  contact: {
+    fontSize: "1em",
+    position: "relative"
   }
+});
 
-  render() {
-    return <div>Header</div>;
-  }
-}
-
-export default Header;
+/*
+  this export statement does a few things
+  exports component to be used elsewhere 
+  connects style object to Header as a prop through withStyles
+  allows history object to be passed to Header through withRouter
+*/
+export default withRouter(withStyles(styles)(Header));
