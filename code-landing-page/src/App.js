b41f2b2adf8458f1af4abe2ae81110c292efd837
[Idrees Ally] <i.m.ally107@gmail.com>
2019-10-06 11:15:40 -0400

    Putting <Header> back into Routing

--- code-landing-page/src/App.js
+++ code-landing-page/src/App.js
@@ -11,9 +11,7 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <Router>
-      {
-        //<Header />
-      }
+      <Header />
       <Route exact path="/" component={Home} />
       <Route exact path="/classes" component={Classes} />
       <Route exact path="/contact" component={Contact} />
