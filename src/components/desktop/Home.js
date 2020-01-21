1975660a6de2870888e4518302f9d6d68c580727
Kentro-lms <syedchowdhury426@live.com>
2020-01-20 23:16:42 -0500

    desktop homepage v1 complete

--- src/components/desktop/Home.js
+++ src/components/desktop/Home.js
@@ -3,6 +3,10 @@ import React from "react";
 import Header from "../desktop/Header";
 
 import homePageBackgroundImage from "../../resources/desktop-background-2.jpg";
+import scrollDownImage from "../../resources/scrollDown.png";
+import facebookIcon from "../../resources/facebook.png";
+import instragramIcon from "../../resources/instagram.png";
+import linkedinIcon from "../../resources/linkedin.png";
 
 const Home = props => {
   return (
@@ -10,12 +14,70 @@ const Home = props => {
       <Header navBarSelection="home" />
       <p style={title}>STUDENTS WHO CODE</p>
       <p style={introText}>
-        We are an after school program committed to inspring a passion for
-        coding in students from an early age
+        We are an <strong>after school program</strong> committed to inspring a{" "}
+        <strong>passion for coding</strong> in students from an{" "}
+        <strong>early age</strong>
       </p>
-      <div style={whatAreWeContainer}></div>
-      <div style={whoAreWeContainer}></div>
-      <div style={socialMediaContainer}></div>
+      <div style={scrollDownContainer}>
+        <a href="#whatAreWe">
+          <img src={scrollDownImage} />
+        </a>
+      </div>
+      <div id="whatAreWe" style={whatAreWesectionStyle}>
+        <p style={whatAreWeHeaderStyle}>What Are We?</p>
+        <div style={whatAreWeBodyStyle}>
+          <p>
+            We are an <strong>afterschool</strong> program with the goal of
+            introducing students to <strong>coding</strong>. Something that they
+            usually would not experience until their <strong>college</strong>{" "}
+            years
+          </p>
+          <p>
+            We work with <strong>educational institutions,</strong>
+            <br /> and also <strong>local communities</strong> to bring this
+            program to their youth
+          </p>
+          <p>
+            StudentsWhoCode is intended for three age demographics:
+            <br /> <strong>elementary</strong>, <strong>middle</strong>, and{" "}
+            <strong>high</strong> school
+          </p>
+        </div>
+      </div>
+      <div style={scrollDownContainer}>
+        <a href="#whoAreWe">
+          <img src={scrollDownImage} />
+        </a>
+      </div>
+      <div id="whoAreWe" style={whoAreWesectionStyle}>
+        <p style={whoAreWeHeaderStyle}>Who Are We?</p>
+        <div style={whoAreWeBodyStyle}>
+          <p>
+            Syed Chowdhury
+            <br />
+            Software Engineer and Product Manager at JPMorgan Chase
+          </p>
+          <p>
+            Idrees Ally
+            <br />
+            Assistant Professor with Rutgers Continuing Education Program at
+            TrilogyEd Bootcamp
+          </p>
+          <p>
+            Tasfiqul Mollah
+            <br />
+            Business Management Student
+          </p>
+        </div>
+      </div>
+      <p>.</p>
+      <div style={socialMediaContainer}>
+        <span>
+          <img style={iconStyle} src={facebookIcon} />
+          <img style={iconStyle} src={instragramIcon} />
+          <img style={iconStyle} src={linkedinIcon} />
+        </span>
+      </div>
     </div>
   );
 };
@@ -24,7 +86,7 @@ const homePageContainerStyles = {
   position: "relative",
   height: "100%",
   backgroundImage: "url(" + homePageBackgroundImage + ")",
-  backgroundSize: "cover",
+  backgroundSize: "100% 95%",
   backgroundRepeat: "no-repeat",
   backgroundAttachment: "fixed"
 };
@@ -41,16 +103,98 @@ const title = {
 const introText = {
   textAlign: "center",
   fontFamily: "roboto",
-  border: "2px solid black",
-  borderRadius: "25px",
-  marginLeft: "30vw",
-  marginRight: "30vw"
+  borderRadius: "30px",
+  backgroundColor: "#13C9EA",
+  color: "#033665",
+  marginTop: "5vh",
+  marginLeft: "27vw",
+  marginRight: "27vw",
+  paddingTop: "10px",
+  paddingBottom: "10px",
+  paddingLeft: "5px",
+  paddingRight: "5px",
+  fontSize: "1.5vw"
 };
 
-const whatAreWeContainer = {};
+const scrollDownContainer = {
+  marginTop: "5vh",
+  textAlign: "center"
+};
 
-const whoAreWeContainer = {};
+const whatAreWesectionStyle = {
+  marginTop: "40vh",
+  display: "flex",
+  flexDirection: "row",
+  flex: 1,
+  justifyContent: "space-evenly"
+};
 
-const socialMediaContainer = {};
+const whatAreWeHeaderStyle = {
+  color: "white",
+  fontWeight: "bold",
+  fontFamily: "futura",
+  fontSize: "3vw",
+  width: "60vw",
+  paddingLeft: "10vw"
+};
+
+const whatAreWeBodyStyle = {
+  fontFamily: "roboto",
+  borderRadius: "30px",
+  backgroundColor: "#13C9EA",
+  color: "#033665",
+  fontSize: "1.2vw",
+  marginLeft: "5vw",
+  marginRight: "5vw",
+  paddingTop: "10px",
+  paddingBottom: "10px",
+  paddingLeft: "5px",
+  paddingRight: "5px"
+};
+
+const whoAreWesectionStyle = {
+  marginTop: "40vh",
+  display: "flex",
+  flexDirection: "row",
+  flex: 1,
+  justifyContent: "space-evenly",
+  marginBottom: "40vh"
+};
+
+const whoAreWeHeaderStyle = {
+  color: "white",
+  fontWeight: "bold",
+  fontFamily: "futura",
+  fontSize: "3vw",
+  width: "50vw",
+  paddingLeft: "10vw"
+};
+
+const whoAreWeBodyStyle = {
+  fontFamily: "roboto",
+  borderRadius: "30px",
+  backgroundColor: "#13C9EA",
+  color: "#033665",
+  fontSize: "1.2vw",
+  marginRight: "5vw",
+  paddingTop: "10px",
+  paddingBottom: "10px",
+  paddingLeft: "5px",
+  paddingRight: "5px"
+};
+
+const socialMediaContainer = {
+  position: "fixed",
+  left: 0,
+  bottom: 0,
+  width: "100vw",
+  backgroundColor: "#5ED1EC",
+  textAlign: "center",
+  height: "5vh"
+};
+
+const iconStyle = {
+  width: "5vh"
+};
 
 export default Home;
