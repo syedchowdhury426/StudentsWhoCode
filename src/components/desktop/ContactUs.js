92dd41ba0d3a05cfadcd3605d738b82d2968a1ac
Idrees Ally <i.m.ally107@gmail.com>
2020-02-15 14:52:15 -0500

    Made changes to contact page desktop

--- src/components/desktop/ContactUs.js
+++ src/components/desktop/ContactUs.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Content } from 'react'
 import * as emailjs from 'emailjs-com'
 //import Layout from '../components/layout'
 import Header from "../desktop/Header";
-import homePageBackgroundImage from "../../resources/desktop-background-2.jpg";
-import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Button, Container, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap';
+//import { StyleSheet } from 'react-native';
+import facebookIcon from "../../resources/facebook.png";
+import instragramIcon from "../../resources/instagram.png";
+import linkedinIcon from "../../resources/linkedin.png";
+
+
 
 class ContactForm extends Component {
   state = {
@@ -48,80 +53,160 @@ class ContactForm extends Component {
 
   render() {
     return (
-      <div style={homePageContainerStyles}>
-        <div>
-          <Header navBarSelection="contact" />
-        </div>
+      <div style={divStyle}>
+        <Header navBarSelection="contact" />
 
-        <div>
-          {/*<Layout>*/}
-          <h1 className="p-heading1">Get in Touch</h1>
+          <p style={mainHeaderText}>Contact Us</p>
+          <Container style={containerStyles}>
           <Form onSubmit={this.handleSubmit.bind(this)}>
 
-            <FormGroup controlId="formBasicEmail">
-              <Label className="text-muted">Email address</Label>
+          <div style={formStyle}>
+            <FormGroup style={formGroupStyle} controlId="formBasicEmail">
+              {/*}<Label className="text-muted">Email address</Label>*/}
               <Input
                 type="email"
                 name="email"
                 value={this.state.email}
                 className="text-primary"
                 onChange={this.handleChange.bind(this, 'email')}
-                placeholder="Enter email"
+                placeholder="Email:"
+                style={inputStyle}
               />
             </FormGroup>
 
-            <FormGroup controlId="formBasicName">
-              <Label className="text-muted">Name</Label>
+            <FormGroup style={formGroupStyle} controlId="formBasicName">
+              {/*}<Label className="text-muted">Name</Label>*/}
               <Input
                 type="text"
                 name="name"
                 value={this.state.name}
                 className="text-primary"
                 onChange={this.handleChange.bind(this, 'name')}
-                placeholder="Name"
+                placeholder="Name:"
+                style={inputStyle}
               />
             </FormGroup>
-
+            </div>
             <FormGroup controlId="formBasicSubject">
-              <Label className="text-muted">Subject</Label>
+              {/*}<Label className="text-muted">Subject</Label>*/}
               <Input
                 type="text"
                 name="subject"
                 className="text-primary"
                 value={this.state.subject}
                 onChange={this.handleChange.bind(this, 'subject')}
-                placeholder="Subject"
+                placeholder="Subject:"
+                style={inputStyle}
               />
             </FormGroup>
 
             <FormGroup controlId="formBasicMessage">
-              <Label className="text-muted">Message</Label>
+              {/*}<Label className="text-muted">Message</Label>*/}
               <Input
                 type="textarea"
                 name="message"
                 className="text-primary"
                 value={this.state.message}
                 onChange={this.handleChange.bind(this, 'message')}
+                placeholder="Body:"
+                style={inputStyle}
               />
             </FormGroup>
 
-            <Button variant="primary" type="submit">
-              Submit
+            <Button style={buttonStyle} variant="primary" type="submit">
+              Send
             </Button>
 
           </Form>
-          {/*</Layout>*/}
-        </div>
+          </Container>
+
+          <div style={socialMediaContainer}>
+            <span>
+              <img style={leftsocialIconStyle} src={linkedinIcon} />
+              <img style={socialIconStyle} src={instragramIcon} />
+              <img style={rightsocialIconStyle} src={facebookIcon} />
+            </span>
+          </div>
       </div>
     )
   }
 }
 
-const homePageContainerStyles = {
-  height: "100vh",
-  backgroundImage: "url(" + homePageBackgroundImage + ")",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat"
+const divStyle ={
+  width:"100vw"
+}
+const containerStyles = {
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    textAlign: "center",
+    borderRadius: "20px",
+    //marginTop: "5%",
+    paddingTop: "5vh",
+    paddingBottom: "2vh",
+    width: "40%",
+    background: "#FFFFFF",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.25)"
+}
+
+const formStyle = {
+  display: 'flex',
+}
+
+const formGroupStyle = {
+  width: "50%"
+}
+
+const inputStyle = {
+  backgroundColor: "#EBF3F5"
+}
+
+const buttonStyle ={
+  borderRadius: "50%",
+  textAlign: "center",
+  padding: "0px",
+  width: "45px",
+  minWidth: "40px",
+  height: "45px",
+  //minHeight: "3vw",
+  backgroundColor: "white",
+  color: "black",
+  fontWeight: "bold",
+  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.25)"
+}
+
+const socialMediaContainer = {
+  //left: 0,
+  bottom: "0px",
+  width: "100%",
+  backgroundColor: "#5ED1EC",
+  textAlign: "center",
+  height: "5vh",
+  //marginTop: "20vh",
+  //paddingTop: "5vh"
+  position: "absolute"
+};
+
+const socialIconStyle = {
+  width: "5vh",
+  marginLeft: "10px",
+  marginRight: "10px"
+};
+
+const leftsocialIconStyle = {
+  width: "5vh"
+};
+
+const rightsocialIconStyle = {
+  width: "5vh"
+};
+
+const mainHeaderText = {
+  textAlign: "center",
+  color: "white",
+  fontWeight: "bold",
+  fontFamily: "Palanquin Dark, sans-serif",
+  fontSize: "3.5vw",
+  paddingTop: "15vh"
 };
 
 export default ContactForm;
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-import DesktopContact from "../components/desktop/ContactUs2";
+import DesktopContact from "../components/desktop/ContactUs";
 import MobileContact from "../components/mobile/ContactUs";
 
+import desktopBackgroundImage from "../resources/desktop-background-2.jpg";
+import mobileBackgroundImage from "../resources/mobile-background.jpg";
+
 const ContactUs = props => {
+
+  const contactPageContainerStyles = {
+    position: "relative",
+    height: "100vh",
+    backgroundImage:
+      "url(" +
+      (props.device === "desktop"
+        ? desktopBackgroundImage
+        : mobileBackgroundImage) +
+      ")",
+    backgroundSize: props.device === "desktop" ? "cover" : "100vw 95vh",
+    position: "fixed",
+    zIndex: -1,
+    overflow: "scroll"
+  };
+
   return (
-    <div style={{ height: "100%" }}>
+    <div id="contact" style={contactPageContainerStyles}>
       {props.device === "desktop" ? <DesktopContact /> : <MobileContact />}
     </div>
   );
